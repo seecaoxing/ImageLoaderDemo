86ec8cdc531a70e602f6e4622aca6c7e781dcc7e
seecaoxing <164607840@qq.com>
2016-08-08 22:22:06 +0800

    第三次提交，分别完成了图片从网络上下载，bitmap流存到磁盘中，以及磁盘路径的设置，通过hash向内存中存数据流，以及最后完善了图片压缩功能其实就是计算inSampleSize。（未测试）

--- app/src/main/java/com/see/imageloaderdemo/ImageResizer.java
+++ app/src/main/java/com/see/imageloaderdemo/ImageResizer.java
@@ -1,7 +1,73 @@
 package com.see.imageloaderdemo;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.view.animation.FastOutLinearInInterpolator;
+import android.util.Log;
+
+import java.io.FileDescriptor;
+import java.sql.SQLInvalidAuthorizationSpecException;
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
+
 /**
+ *
  * Created by see on 2016/8/6.
  */
 public class ImageResizer {
+    private static final String TAG = "ImageResizer";
+
+    public ImageResizer() {
+    }
+
+    public Bitmap decodeSampledBitmapFromResource(Resources res, int resId, int reqWidth, int reqHeight) {
+
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+
+        options.inJustDecodeBounds = true;//仅仅返回尺寸
+        BitmapFactory.decodeResource(res, resId, options);
+
+        //计算 inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+        //通过inSampleSize的设置，生成Bitmap
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+
+
+    }
+
+    private int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        if (reqWidth == 0 || reqHeight == 0) {
+            return 1;
+        }
+        //image原来的 height 和 width
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        Log.d(TAG, "origin,w=" + width + " h=" + height);
+        int inSampleSize = 1;
+        if (height > reqHeight || width > reqWidth) {
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+            //计算出来insamplesize的值是2的幂，并且让高度和宽度大于reqHeight和reqWidth
+            while ((halfHeight / inSampleSize) >= reqHeight && (halfHeight / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+
+        }
+        Log.d(TAG, "sampleSize:" + inSampleSize);
+        return inSampleSize;
+    }
+
+    public Bitmap decodeSampledBitmapFromFileDescriptor(FileDescriptor fd, int reqWidth, int reqHeight) {
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFileDescriptor(fd, null, options);
+
+        //计算 inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+        //通过 inSampleSize的设置生成bitmap
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFileDescriptor(fd, null, options);
+
+    }
 }
