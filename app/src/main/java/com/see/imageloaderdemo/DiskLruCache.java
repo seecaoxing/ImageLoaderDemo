f95be97543ea6d72780139813cc8034e6d38bc40
seecaoxing <164607840@qq.com>
2016-08-08 10:13:45 +0800

    第二次提交，导入DiskLruCache。

--- app/src/main/java/com/see/imageloaderdemo/DiskLruCache.java
+++
@@ -1,7 +0,0 @@
-package com.see.imageloaderdemo;
-
-/**
- * Created by see on 2016/8/6.
- */
-public class DiskLruCache {
-}
--- app/src/main/java/com/see/imageloaderdemo/LoaderResult.java
+++ app/src/main/java/com/see/imageloaderdemo/LoaderResult.java
@@ -1,7 +1,14 @@
 package com.see.imageloaderdemo;
 
+import android.graphics.Bitmap;
+import android.widget.ImageView;
+
 /**
  * Created by see on 2016/8/6.
  */
 public class LoaderResult {
+    public LoaderResult(ImageView imageView, String uri, Bitmap bitmap) {
+
+
+    }
 }
